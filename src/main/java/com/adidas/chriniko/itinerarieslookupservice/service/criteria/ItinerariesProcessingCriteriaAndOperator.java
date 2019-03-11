4cadea2514f1d59c0bd93856df654e357d6f0227
Nikolaos Christidis <nick.christidis@glispamedia.com>
2019-03-11 02:50:50 +0200

    minor change

--- src/main/java/com/adidas/chriniko/itinerarieslookupservice/service/criteria/ItinerariesProcessingCriteriaAndOperator.java
+++ src/main/java/com/adidas/chriniko/itinerarieslookupservice/service/criteria/ItinerariesProcessingCriteriaAndOperator.java
@@ -1,6 +1,8 @@
 package com.adidas.chriniko.itinerarieslookupservice.service.criteria;
 
 
+import com.adidas.chriniko.itinerarieslookupservice.domain.Itinerary;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,5 +12,20 @@ public abstract class ItinerariesProcessingCriteriaAndOperator implements Itiner
 
     public ItinerariesProcessingCriteriaAndOperator(ItinerariesProcessingCriteria... criteria) {
         processingCriteria = Arrays.asList(criteria);
+    }
+
+    @Override
+    public List<Itinerary> process(List<Itinerary> input) {
+
+        ItinerariesProcessingCriteria head = processingCriteria.get(0);
+        List<ItinerariesProcessingCriteria> tail = processingCriteria.subList(1, processingCriteria.size());
+
+        List<Itinerary> result = head.process(input);
+
+        for (ItinerariesProcessingCriteria processingCriteria : tail) {
+            result = processingCriteria.process(result);
+        }
+
+        return result;
     }
 }
--- src/main/java/com/adidas/chriniko/itinerarieslookupservice/service/criteria/LessConnectionsAndLessTimeItinerariesProcessingCriteria.java
+++ src/main/java/com/adidas/chriniko/itinerarieslookupservice/service/criteria/LessConnectionsAndLessTimeItinerariesProcessingCriteria.java
@@ -1,10 +1,7 @@
 package com.adidas.chriniko.itinerarieslookupservice.service.criteria;
 
-import com.adidas.chriniko.itinerarieslookupservice.domain.Itinerary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class LessConnectionsAndLessTimeItinerariesProcessingCriteria extends ItinerariesProcessingCriteriaAndOperator {
@@ -18,20 +15,5 @@ public class LessConnectionsAndLessTimeItinerariesProcessingCriteria extends Iti
     @Override
     public String itinerariesCriteriaName() {
         return "less-connections-and-less-time";
-    }
-
-    @Override
-    public List<Itinerary> process(List<Itinerary> input) {
-
-        ItinerariesProcessingCriteria head = processingCriteria.get(0);
-        List<ItinerariesProcessingCriteria> tail = processingCriteria.subList(1, processingCriteria.size());
-
-        List<Itinerary> result = head.process(input);
-
-        for (ItinerariesProcessingCriteria processingCriteria : tail) {
-            result = processingCriteria.process(result);
-        }
-
-        return result;
     }
 }
